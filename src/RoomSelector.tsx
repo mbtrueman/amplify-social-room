4b6ad44ff0d899be003330491cf1c9a070f7c53a
Miko Trueman <mbtrueman@gmail.com>
2024-08-28 18:02:01 -0700

    Add multi-cursor support.

--- src/RoomSelector.tsx
+++ src/RoomSelector.tsx
@@ -1,6 +1,9 @@
-import { Schema } from "../amplify/data/resource"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { Schema } from "../amplify/data/resource"
 import { defaultRoom } from "./utils";
 import { useEffect, useState } from "react";
+import { type Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
 export function RoomSelector({
   currentRoomId,
@@ -10,12 +13,24 @@ export function RoomSelector({
   onRoomChange: (roomId: string) => void
 }) {
 
-  const [rooms, setRooms] = useState<Schema["Room"]["type"][]>([defaultRoom])
+  const [rooms, setRooms] = useState<Schema["Room"]["type"][]>([defaultRoom]);
+  const [client, setClient] = useState<any>();
   
   useEffect(() => {
     // Add observeQuery code here
-  }, [])
+    const _client = generateClient<Schema>();
+    // console.log('client:', client);
+    const sub = _client.models.Room.observeQuery().subscribe({
+      next: (data) => {
+        setRooms([defaultRoom, ...data.items])
+      }
+    });
 
+    setClient(_client);
+    return () => sub.unsubscribe()
+  }, []);
+
+  console.log(rooms);
   return <>
     <select
       onChange={e => onRoomChange(e.target.value)}
@@ -23,7 +38,17 @@ export function RoomSelector({
       {rooms.map(room => <option value={room.id} key={room.id}>{room.topic}</option>)}
     </select>
     <button onClick={async () => {
-      // Add create Room logic here
+      const newRoomName = window.prompt("Room name")
+      if (!newRoomName) {
+        return
+      }
+      const { data: room } = await client.models.Room.create({
+        topic: newRoomName
+      })
+      
+      if (room !== null) {
+        onRoomChange(room.id)
+      }
     }}>[+ add]</button>
   </>
 }
